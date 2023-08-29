6dd6787b1b94c758b778a41596a11bb3420e528a
Burak Büyük <cp.burakbuyuk@gmail.com>
2023-08-29 20:59:55 +0300

    Commit
    UI katmanı için tema düzenlemeleri

--- RealEstate_Dapper_Api/RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
+++ RealEstate_Dapper_Api/RealEstate_Dapper_UI/obj/Debug/net8.0/RealEstate_Dapper_UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201130cb487c4f47c16f32b4877a0256cede2381")]
 [assembly: System.Reflection.AssemblyProductAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RealEstate_Dapper_UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
